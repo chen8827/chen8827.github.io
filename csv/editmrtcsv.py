65806c72338564c0d06be95a04d5dd7c5a0d57c3
chen8827 <87763673+chen8827@users.noreply.github.com>
2021-07-31 00:10:57 +0800

    Delete editmrtcsv.py

--- csv/editmrtcsv.py
+++
@@ -1,7 +0,0 @@
-# add index column
-import pandas as pd
-path='mrt.csv'
-mrt=pd.read_csv('mrt.csv',header=0)
-mrt=mrt.rename_axis('index').reset_index()
-store=pd.DataFrame(mrt)
-store.to_csv(path,encoding='utf-8')
